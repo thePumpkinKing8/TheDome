9c0af406189b01ea02d6181335adbf8ccf5d5b2f
Blake Laprade <lapradeblake@gmail.com>
2025-01-20 16:53:09 -0700

    smoothed physics of grabbed objects and camera

--- The Dome/Assets/Scripts/Objects/Grabbable.cs
+++ The Dome/Assets/Scripts/Objects/Grabbable.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Grabbable : MonoBehaviour
 {
     protected bool _grabbed = false;
+    private Rigidbody2D _rb;
+    private Vector3 _velector;
+    private float _vel;
+    private Vector3 _pastPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,7 +20,11 @@ public class Grabbable : MonoBehaviour
     {
         if(_grabbed)
         {
+            _pastPos = transform.position;
             transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            _velector = (transform.position - _pastPos);
+            _vel = (_pastPos - transform.position).magnitude / Time.deltaTime;
+            _velector *= _vel;
         }
     }
 
@@ -26,13 +34,14 @@ public class Grabbable : MonoBehaviour
             return;
         _grabbed = true;
         PlayerManager.Instance.SetGrabbedObject(this);
-
+        _pastPos = transform.position;
         Debug.Log("grabbed");
     }
 
     public virtual void GrabEnd()
     {
         _grabbed = false;
+        _rb.velocity = _velector;
         Debug.Log("ungrabbed");
     }
 }
